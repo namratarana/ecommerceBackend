08d41c7532eb0bd87d632e4e74ec38112584f1be
Tejas Borkar <tvborkar1@gmail.com>
2021-08-06 12:02:59 +0530

    abhi

--- route.js
+++ route.js
@@ -2,7 +2,7 @@ const Express = require('express');
 const productController = require('./controllers/products.controller');
 const productRouter = Express.Router();
 
-productRouter.get('/product/:id', productController.fetchById)
+productRouter.get('/:id', productController.fetchById)
 productRouter.get('/categories', productController.fetchProductCategory)
 productRouter.get('/new', productController.fetchNewproducts)
 productRouter.get('/popular', productController.fetchPopularProducts)
